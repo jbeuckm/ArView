ae40f513aa79397150d27d22b32d7eb0585f55ad
Joe Beuckman <joseph.beuckman@usbank.com>
2013-08-21 15:02:33 -0500

    split out location utilities into a module - might be temporary

--- controllers/widget.js
+++ controllers/widget.js
@@ -1,27 +1,40 @@
 var args = arguments[0] || {};
 
+var acc = require(WPATH('accelerometer'));
+var location_utils = require(WPATH("location_utils"));
 
-var isAndroid = Ti.Platform.osname == 'android';
+
+var deviceLocation = null;
+var deviceBearing = 1;
+
+var isAndroid = Ti.Platform.osname == "android";
 
 var screenWidth = Ti.Platform.displayCaps.platformWidth;
 var screenHeight = Ti.Platform.displayCaps.platformHeight;
-
 var halfScreenHeight = screenHeight / 2;
 var halfScreenWidth = screenWidth / 2;
 
-var overlay = $.overlay;
-overlay.height = screenHeight;
-overlay.width = screenWidth;
-// view large enough to rotate 90deg without seeing edges
-$.arContainer.height = 1.2 * screenHeight;
-$.arContainer.width = 1.2 * screenHeight;
-
-var d2rFactor = Math.PI / 180;
 
 var MAX_POI_COUNT = 25;
+/*
 var MIN_Y = Math.floor(screenHeight / 6);
 var MAX_Y = Math.floor(screenHeight / 4 * 3);
 var DELTA_Y = MAX_Y - MIN_Y;
+*/
+var limitLeft = -halfScreenHeight - 100;
+var limitRight = halfScreenHeight + 100;
+
+var lowY = halfScreenWidth * .8;
+var highY = -halfScreenWidth * .8;
+var yRange = highY - lowY;
+
+
+
+$.overlay.height = screenHeight;
+$.overlay.width = screenWidth;
+// view large enough to rotate 90deg without seeing edges
+$.arContainer.height = 1.2 * screenHeight;
+$.arContainer.width = 1.2 * screenHeight;
 
 // Setup the location  properties for callbacks
 Ti.Geolocation.headingFilter = 1;
@@ -43,22 +56,21 @@ var stability = .6;
 var volatility = 1 - stability;
 var PI_2 = Math.PI/2;
 
-var pitch; // from 90 (looking straight up) to -90 (looking straight down)
-var roll;
+var devicePitch = 0; // from 90 (looking straight up) to -90 (looking straight down)
+var deviceRoll = 0;
 
 function accelerationHandler(e) {
 
-	pitch = e.z * 90;
-	roll = radians2Degrees(Math.atan2(e.y, e.x));
+	devicePitch = e.z * 90;
+	deviceRoll = location_utils.radians2Degrees(Math.atan2(e.y, e.x));
 
-$.pitchLabel.text = pitch.toPrecision(3);
-$.rollLabel.text = roll.toPrecision(3);
+$.pitchLabel.text = devicePitch.toPrecision(3);
+$.rollLabel.text = deviceRoll.toPrecision(3);
 	
-	yOffset = stability * yOffset + volatility * (halfScreenHeight * (pitch + PI_2));
+	yOffset = stability * yOffset + volatility * (halfScreenHeight * (devicePitch + PI_2));
 	
 	updatePoiViews();
 }
-var acc = require(WPATH('accelerometer'));
 acc.setupCallback(accelerationHandler);
 acc.start();
 
@@ -83,23 +95,14 @@ function showAR() {
 		autohide : false,
 		autofocus : "off",
 		animated : false,
-		overlay : overlay,
+		overlay : $.overlay,
 		transform: cameraTransform
 	});
 }
 
 
-
-var deviceLocation = null;
-var deviceBearing = 1;
-
-
-var arContainer = $.arContainer;
-var headingLabel = $.headingLabel;
-var radar = $.radarView;
-
 if (args.overlay) {
-	overlay.add(args.overlay);
+	$.overlay.add(args.overlay);
 }
 
 
@@ -107,7 +110,7 @@ if (args.overlay) {
 // iPhone camera sees about 30 degrees left to right
 var FIELD_OF_VIEW_HORIZONTAL = 30;
 
-var maxRange = 1000;
+var maxRange = 10000;
 
 if (args.maxDistance) {
 	maxRange = args.maxDistance;
@@ -155,7 +158,7 @@ function cullDistantPois(_pois, MAX_COUNT) {
 
 	for (i=0, l=_pois.length; i<l; i++) {
 		var poi = _pois[i];
-		var d = calculateDistance(deviceLocation, poi);
+		var d = location_utils.calculateDistance(deviceLocation, poi);
 		poi.distance = d;
 	}
 
@@ -176,7 +179,17 @@ function cullDistantPois(_pois, MAX_COUNT) {
  * @param {Object} e
  */
 function locationCallback(e) {
-Ti.API.info('locationCallback()');
+
+	//default to minneapolis in the simulator
+	if (Ti.Platform.model == "Simulator") {
+		e.coords = {
+			latitude: 44.9769873,
+			longitude: -93.2681625,
+			speed: 0
+		};
+	}
+
+Ti.API.info('locationCallback('+e.coords.latitude+', '+e.coords.longitude+')');
 	deviceLocation = e.coords;
 	
 	if (!deviceLocation) {
@@ -207,10 +220,10 @@ function headingCallback(e) {
 	deviceBearing = e.heading.trueHeading;
 
 	// REM this if you don't want the user to see their heading
-	headingLabel.text = Math.floor(deviceBearing) + "\xB0";
+	$.headingLabel.text = Math.floor(deviceBearing) + "\xB0";
 
 	// point the radar view
-	radar.transform = Ti.UI.create2DMatrix().rotate(-1 * deviceBearing);
+	$.radarView.transform = Ti.UI.create2DMatrix().rotate(-1 * deviceBearing);
 	
 	updatePoiViews();
 	
@@ -236,18 +249,19 @@ function updateRelativePositions() {
 		
 		if (poi.view) {
 
-			poi.distance = calculateDistance(deviceLocation, poi);
+			poi.distance = location_utils.calculateDistance(deviceLocation, poi);
 			
 			// this would ideally be more of a databinding event
 			poi.controller.setDistance(Math.floor(poi.distance)+'m');
-			
-			if (maxRange && (poi.distance <= maxRange) ) {
+
+			if (poi.distance <= maxRange) {
 				
 				maxPoiDistance = Math.max(maxPoiDistance, poi.distance);
 				minPoiDistance = Math.min(minPoiDistance, poi.distance);
 				
 				poi.inRange = true;
-				poi.bearing = calculateBearing(deviceLocation, poi);
+
+				poi.bearing = location_utils.calculateBearing(deviceLocation, poi);
 				
 				positionRadarBlip(poi);
 
@@ -276,20 +290,11 @@ function updateRelativePositions() {
 
 }
 
-var limitLeft = -halfScreenWidth - 100;
-var limitRight = +halfScreenWidth + 100;
-
-var lowY = screenHeight/2 * .8;
-var highY = -screenHeight/2 * .8;
-var yRange = highY - lowY;
-
-// container for pivoting the scene according to device orientation
-var gimbal = $.gimbal;
 
 function updatePoiViews() {
 	
 	var gimbalTransform = Ti.UI.create2DMatrix();
-	gimbal.transform = gimbalTransform.rotate(-roll - 90);
+	$.gimbal.transform = gimbalTransform.rotate(-deviceRoll - 90);
 
 	for (i=0, l=pois.length; i<l; i++) {
 
@@ -299,7 +304,7 @@ function updatePoiViews() {
 
 			poi.blip.visible = true;
 
-			var horizontalPositionInScene = projectBearingIntoScene(poi.bearing);
+			var horizontalPositionInScene = horizontalPositionFromBearing(poi.bearing);
 
 			if ((horizontalPositionInScene > limitLeft) && (horizontalPositionInScene < limitRight)) {
 				poi.view.visible = true;
@@ -311,6 +316,8 @@ function updatePoiViews() {
 
 				var y = lowY + distanceRank * yRange + yOffset;
 				// this translation is from the center of the screen
+//Ti.API.info(horizontalPositionInScene);
+//				transform = transform.translate(horizontalPositionInScene, 0);
 				transform = transform.translate(horizontalPositionInScene, y);
 
 
@@ -360,14 +367,14 @@ function createRadarBlips() {
 		poi.blip = require('/alloy').createWidget('ArView', 'blip', {}).getView();
 		positionRadarBlip(poi);		
 		// add to blip to the radar
-		radar.add(poi.blip);
+		$.radarView.add(poi.blip);
 	}
 }
 
 
 function positionRadarBlip(poi) {
 
-	var rad = degrees2Radians(poi.bearing);
+	var rad = location_utils.degrees2Radians(poi.bearing);
 
 	var relativeDistance = poi.distance / (maxRange * 1.2);
 	var x = (40 + (relativeDistance * 40 * Math.sin(rad)));
@@ -384,73 +391,11 @@ function positionRadarBlip(poi) {
  * 
  * @param {Object} poiBearing
  */
-function projectBearingIntoScene(poiBearing) {
-	var delta = findAngularDistance(poiBearing, deviceBearing);
+function horizontalPositionFromBearing(poiBearing) {
+	var delta = location_utils.findAngularDistance(poiBearing, deviceBearing);
 	return delta * screenWidth / FIELD_OF_VIEW_HORIZONTAL;
 }
 
-
-/**
- * Finds the separation between angles theta1 and theta2 (in degrees)
- */
-function findAngularDistance(theta1, theta2) {
-	var a = theta1 - theta2;
-	if (a > 180) a -= 360;
-	if (a < -180) a += 360;
-	return a;
-}
-
-function degrees2Radians(val) {
-	return val * d2rFactor;
-}
-function radians2Degrees(val) {
-	return val / d2rFactor;
-}
-
-/**
- * Which direction to get from point1 to point2?
- * @param {Object} point1
- * @param {Object} point2
- */
-function calculateBearing(point1, point2) {
-	var lat1 = degrees2Radians(point1.latitude);
-	var lat2 = degrees2Radians(point2.latitude);
-	var dlng = degrees2Radians((point2.longitude - point1.longitude));
-	var y = Math.sin(dlng) * Math.cos(lat2);
-	var x = Math.cos(lat1) * Math.sin(lat2) - Math.sin(lat1) * Math.cos(lat2) * Math.cos(dlng);
-	var brng = Math.atan2(y, x);
-	return ((brng * (180 / Math.PI)) + 360) % 360;
-};
-
-/**
- * Find geo-distance betwixt two locations on earth's surface
- * @param {Object} loc1
- * @param {Object} loc2
- */
-function calculateDistance(loc1, loc2) {
-	
-	if (!loc1.latitude || (typeof loc1.latitude != "number")) {
-		throw(new Error("error reading loc1.latitude"));
-	}
-	if (!loc1.longitude || (typeof loc1.longitude != "number")) {
-		throw(new Error("error reading loc1.longitude"));
-	}
-	if (!loc2.latitude || (typeof loc2.latitude != "number")) {
-		throw(new Error("error reading loc2.latitude"));
-	}
-	if (!loc2.longitude || (typeof loc2.longitude != "number")) {
-		throw(new Error("error reading loc2.longitude"));
-	}
-
-	var dLat = degrees2Radians(loc2.latitude - loc1.latitude);
-	var dLon = degrees2Radians(loc2.longitude - loc1.longitude);
-
-	var R = 6371;	// Radius of the earth in km
-	var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) + Math.cos(degrees2Radians(loc1.latitude)) * Math.cos(degrees2Radians(loc2.latitude)) * Math.sin(dLon / 2) * Math.sin(dLon / 2);
-	var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-	// Distance in m
-	return R * c * 1000;
-};
 
 
 
@@ -486,10 +431,6 @@ function closeAndDestroy() {
 }
 
 
-exports.findAngularDistance = findAngularDistance;
-exports.calculateDistance = calculateDistance;
-exports.calculateBearing = calculateBearing;
-exports.degrees2Radians = degrees2Radians;
 exports.closeAndDestroy = closeAndDestroy;
 
 exports.cullDistantPois = cullDistantPois;
